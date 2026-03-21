5b9c59990f245643935cfe5e1f93f129c048a0b3
Adnan Elahi <adnanelahi.nsu@gmail.com>
2026-03-21 18:48:49 -0400

    setup file updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author='Adnan Elahi',
+    author_email='mradnanelahi@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
