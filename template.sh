02dd2ea59794dbe23610157e2054e6975be6bd7d
Adnan Elahi <adnanelahi.nsu@gmail.com>
2026-03-21 18:29:42 -0400

    Folder structure updated

---
+++ template.sh
@@ -0,0 +1,13 @@
+#creating directory for the project
+mkdir -p src
+
+#creating files for the project
+touch src/_init_.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch store_index.py
+
+echo "Directory and files created successfully!"
